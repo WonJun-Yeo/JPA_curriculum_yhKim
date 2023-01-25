fe26d61fae77d9c0661bc38fee22098ee9cda552
WonJun Yeo <rbgh004@gmail.com>
2023-01-25 23:20:03 +0900

    <feat> Member domain development

---
+++ jpashop/src/main/java/japbook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package japbook.jpashop.service;
+
+import japbook.jpashop.domain.Member;
+import japbook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    // Join
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // findAll Member
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // findMemberById
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
