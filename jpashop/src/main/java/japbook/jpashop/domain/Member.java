442b0befb21f16a9e689fa67f5c34b36fbe6836b
WonJun Yeo <rbgh004@gmail.com>
2023-01-25 17:43:52 +0900

    <feat> add Association Convenience Method

--- jpashop/src/main/java/japbook/jpashop/domain/Member.java
+++ jpashop/src/main/java/japbook/jpashop/domain/Member.java
@@ -1,10 +1,14 @@
 package japbook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
     @Id
     @GeneratedValue
--- jpashop/src/main/java/japbook/jpashop/domain/Order.java
+++ jpashop/src/main/java/japbook/jpashop/domain/Order.java
@@ -16,19 +16,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime orderDate; // oderTime
+    private LocalDateTime orderDate; // orderTime
 
     @Enumerated(EnumType.STRING)
     private OderStatus status; // orderState [ORDER, CANCEL]
+
+
+    //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery (Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
