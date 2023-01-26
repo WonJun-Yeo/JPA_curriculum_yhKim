5bd8383c45287b7e3fbd76caa62a845c4e7bb14e
WonJun Yeo <rbgh004@gmail.com>
2023-01-26 10:10:27 +0900

    <feat> Item domain development

--- jpashop/src/main/java/japbook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/japbook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package japbook.jpashop.domain.item;
 
 import japbook.jpashop.domain.Category;
+import japbook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //==biz logic==//
+
+    // stock increase
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock decrease
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
