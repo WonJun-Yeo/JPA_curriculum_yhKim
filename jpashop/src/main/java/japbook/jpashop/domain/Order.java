676e13872ff38e0321b130ad3dcf4be884f4792b
WonJun Yeo <rbgh004@gmail.com>
2023-01-26 10:36:56 +0900

    <feat> order and orderItem entity development

--- jpashop/src/main/java/japbook/jpashop/domain/Order.java
+++ jpashop/src/main/java/japbook/jpashop/domain/Order.java
@@ -30,7 +30,7 @@ public class Order {
     private LocalDateTime orderDate; // orderTime
 
     @Enumerated(EnumType.STRING)
-    private OderStatus status; // orderState [ORDER, CANCEL]
+    private OrderStatus status; // orderState [ORDER, CANCEL]
 
 
     //
@@ -47,5 +47,40 @@ public class Order {
     public void setDelivery (Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+
+    //== create method ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderDate(LocalDateTime.now());
+        order.setStatus(OrderStatus.ORDER);
+        return order;
+    }
+
+    //== biz logic ==//
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송이 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- jpashop/src/main/java/japbook/jpashop/domain/OrderStatus.java
+++ jpashop/src/main/java/japbook/jpashop/domain/OrderStatus.java
@@ -1,5 +1,5 @@
 package japbook.jpashop.domain;
 
-public enum OderStatus {
+public enum OrderStatus {
     ORDER, CANCEL
 }
