6624004f96fe01a347a5e4bbe8a1db90010de210
WonJun Yeo <rbgh004@gmail.com>
2023-01-27 10:16:05 +0900

    <feat> add home layout

---
+++ jpashop/src/main/java/japbook/jpashop/controller/HomeController.java
@@ -0,0 +1,20 @@
+package japbook.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+      return "home";
+    }
+
+
+
+}
