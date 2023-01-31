9392a4c6347d7f8e8bd4f442d2cc788836368e15
WonJun Yeo <rbgh004@gmail.com>
2023-01-31 11:18:21 +0900

    <feat> add member update api

--- jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/japbook/jpashop/api/MemberApiController.java
@@ -2,14 +2,12 @@ package japbook.jpashop.api;
 
 import japbook.jpashop.domain.Member;
 import japbook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -21,6 +19,7 @@ public class MemberApiController {
   //
   private final MemberService memberService;
 
+  // member join api
   @PostMapping("api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
     Long id = memberService.join(member);
@@ -37,6 +36,21 @@ public class MemberApiController {
     return new CreateMemberResponse(id);
   }
 
+  // member modify api
+  @PutMapping("api/v2/members/{id}")
+  public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                             @RequestBody @Valid UpdateMemberRequest request) {
+    // command, query 분리
+    memberService.update(id, request.getName());
+    Member findMember = memberService.findOne(id);
+    return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+  }
+
+  /**
+   * DTO
+   * entity 를 직접 받고 반환해주게 되면 entity 변경시 api spec 이 변하게되어 side effect 가 심하게 발행할 수 있다.
+   * */
+  // join
   @Data
   static class CreateMemberResponse {
     private Long id;
@@ -52,4 +66,17 @@ public class MemberApiController {
     private String name;
   }
 
+  // update
+
+  @Data
+  @AllArgsConstructor
+  static class UpdateMemberResponse {
+    private Long id;
+    private String name;
+  }
+
+  @Data
+  static class UpdateMemberRequest {
+    private String name;
+  }
 }
